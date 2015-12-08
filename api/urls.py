8c3c1d8a356e460df063cbe3ddbd464c5d1da017
cesarmarroquin <cesarm2333@gmail.com>
2015-12-08 14:51:31 -0800

    added routes for api

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
-# from api.views import *
-# from django.conf.urls import url, include
-# from rest_framework.urlpatterns import format_suffix_patterns
-# from rest_framework.authtoken import views
-#
-# urlpatterns = [
-#     url(r'^parents/$', ListParents.as_view(), name='list_parents'),
-#     url(r'^parents/(?P<pk>\d+)$', DetailParents.as_view(), name='detail_parents'),
-#     url(r'^teachers/$', ListTeachers.as_view(), name='list_teachers'),
-#     url(r'^teachers/(?P<pk>\d+)$', DetailTeachers.as_view(), name='detail_teachers'),
-#
-# ]
-#
-# urlpatterns = format_suffix_patterns(urlpatterns)
+from api.views import *
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views
+
+urlpatterns = [
+    url(r'^parents/$', ListParents.as_view(), name='list_parents'),
+    url(r'^parents/(?P<pk>\d+)$', DetailParents.as_view(), name='detail_parents'),
+    url(r'^teachers/$', ListTeachers.as_view(), name='list_teachers'),
+    url(r'^teachers/(?P<pk>\d+)$', DetailTeachers.as_view(), name='detail_teachers'),
+
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
