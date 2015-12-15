a60b52b32f930db6407ce209626de95d42e44d0f
cesarmarroquin <cesarm2333@gmail.com>
2015-12-15 10:30:38 -0800

    extended the number of api results to 10

--- teacher_parent/settings.py
+++ teacher_parent/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
