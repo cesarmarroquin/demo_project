a55fd36d39e69ea864d2af6711a3e7ecbaf484b1
cesarmarroquin <cesarm2333@gmail.com>
2015-12-29 22:37:02 -0800

    created django command that creates attendance object every day for a student

---
+++ schools/management/commands/attendance.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from schools.models import *
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    args = 'Arguments is not needed'
+    help = 'Django admin custom command poc.'
+
+    def handle(self, *args, **options):
+        for school_class in SchoolClass.objects.all():
+            for student in Student.objects.filter(school_class=school_class):
+                StudentAttendance.objects.create(student=student, school_class=school_class)
