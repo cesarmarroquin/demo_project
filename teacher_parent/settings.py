a286dd8c4fae234f35a51af5b8f32617ec8179a8
cesarmarroquin <cesarm2333@gmail.com>
2016-01-06 11:16:20 -0800

    configured environment variable

--- teacher_parent/settings.py
+++ teacher_parent/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3fnf)521mz@=-$%f6xx(m!m!$ytlgttq4n&wixlj0w4eh1=r*&'
-
+# SECRET_KEY = '3fnf)521mz@=-$%f6xx(m!m!$ytlgttq4n&wixlj0w4eh1=r*&'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
